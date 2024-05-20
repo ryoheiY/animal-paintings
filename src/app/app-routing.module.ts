7ed273e249801c08e7d8f5cd8bf608cdea28f218
ryoheiY <momoyamashia@icloud.com>
2024-05-20 09:41:03 +0900

    init処理を追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/models/product.ts
@@ -0,0 +1,6 @@
+export class Product {
+    id: number = 0;
+    name: string = "";
+    prica: number = 0;
+    image_url: string = "";
+}
