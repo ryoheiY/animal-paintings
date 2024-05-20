b3e0f3f4e6b0d15d9559e5599ecfcaaac6c42172
ryoheiY <momoyamashia@icloud.com>
2024-05-20 14:27:52 +0900

    Styleを修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,6 +1,6 @@
 export class Product {
     id: number = 0;
     name: string = "";
-    prica: number = 0;
+    price: number = 0;
     image_url: string = "";
 }
